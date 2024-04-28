9c22878d9734a7ee2ea1109934ab6589644fe27b
GokuBlack83 <167849226+GokuBlack83@users.noreply.github.com>
2024-04-28 02:09:17 -0600

    Update game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 const timeout = 60000;
-const poin = 500;
+const poin = 5000;
 const handler = async (m, {conn, usedPrefix}) => {
   conn.tekateki = conn.tekateki ? conn.tekateki : {};
   const id = m.chat;
